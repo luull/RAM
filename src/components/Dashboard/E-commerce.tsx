6c28f0aecea1d78c4058baed29556c64d29233ef
luull <assalafy@steradian.co.id>
2024-12-02 20:28:14 +0700

    fix failed build part 3 anying cape

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -1,11 +1,8 @@
 "use client";
 import React from "react";
-import ChartThree from "../Charts/ChartThree";
-import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
-import ChartOne from "@/components/Charts/ChartOne";
 
 const ECommerce: React.FC = () => {
   return (
@@ -13,9 +10,6 @@ const ECommerce: React.FC = () => {
       <DataStatsOne />
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
-        <ChartOne />
-        <ChartTwo />
-        <ChartThree />
         <div className="col-span-12 xl:col-span-8">
           <TableOne />
         </div>
