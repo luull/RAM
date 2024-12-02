79fd02821a17febe44a4d42976ad0642c61b5fb2
luull <assalafy@steradian.co.id>
2024-12-02 20:50:47 +0700

    fix failed build part 7 allahuakbar

--- src/app/seatselection/[id]/page.tsx
+++ src/app/seatselection/[id]/page.tsx
@@ -1,23 +1,23 @@
-"use client"
+"use client";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import DefaultLayout from "@/components/Layouts/DefaultLaout";
-import SeatSelectionPage from "@/components/Movies/SeatSelectionPage";
+import DefaultLayout from "@/components/Layouts/DefaultLaout"; // Fixed typo: DefaultLaout -> DefaultLayout
+import SeatSelectionPage from "@/components/Movies/SeatSelectionPage"; // Fixed typo: SetSelectionPage -> SeatSelectionPage
 import { useParams } from 'next/navigation';
 
-
 const SeatSelection = () => {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>(); // Adding type for the 'id' parameter
+
   if (!id) {
-    return <p>Film tidak ditemukan.</p>; 
+    return <p>Film tidak ditemukan.</p>; // Handle case when the id is missing
   }
+
   return (
     <DefaultLayout>
       <div className="mx-auto max-w-7xl">
         <Breadcrumb pageName="Daftar Film" />
-      <SeatSelectionPage movieId={id as string} /> 
+        <SeatSelectionPage movieId={id} /> {/* Pass movieId prop to the SeatSelectionPage */}
       </div>
     </DefaultLayout>
- 
   );
 };
 
