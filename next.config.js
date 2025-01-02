de97eb568aa6b506caf73c471af21bbcdd0a5adf
luull <assalafy@steradian.co.id>
2025-01-02 11:20:50 +0700

    change next config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/RAM', // Required for GitHub Pages deployment
-  assetPrefix: '/RAM', // Ensures assets are prefixed correctly
-  trailingSlash: true, // Ensures paths end with a slash
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '/RAM', // Makes it accessible in the app
-  },
-  images: {
-    loader: 'default',
-    path: '/RAM/_next/image', // Adjust the path for GitHub Pages
-    domains: ["image.tmdb.org"], // For external images
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
